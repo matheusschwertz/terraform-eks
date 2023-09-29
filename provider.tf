ab0052b6549f4e913b467b8b59502c64de106b7f
matheusschwertz <matheusschwertz@gmail.com>
2023-09-28 21:20:21 -0300

    feat: adding remote backend

--- provider.tf
+++ provider.tf
@@ -1,9 +1,15 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.17.0"
     }
+  }
+  backend "s3" {
+    bucket = "eks-comunidadedevops"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
+
   }
 }
 
