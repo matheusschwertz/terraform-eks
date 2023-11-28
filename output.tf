fcb811cd921db30eaf75e601b6e9d153e50d0474
matheusschwertz <matheusschwertz@gmail.com>
2023-11-27 23:07:58 -0300

    fix Adding outpout to model

---
+++ output.tf
@@ -0,0 +1,11 @@
+output "ca" {
+  value = module.eks_cluster.certificate_authority
+}
+
+output "endpoint" {
+  value = module.eks_cluster.endpoint
+}
+
+output "cluster_name" {
+  value = module.eks_cluster.cluster_name
+}
